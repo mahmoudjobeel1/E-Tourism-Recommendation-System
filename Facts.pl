0f74a888eb85e9e34768db0fa830d50dd2217951
MahmoudJobeel <64175028+mahmoudjobeel1@users.noreply.github.com>
2020-05-18 00:20:08 +0200

    Add files via upload

---
+++ Facts.pl
@@ -0,0 +1,29 @@
+
+% offerMean(X, Y) -> Transportation mean Y is used with offer X
+
+offerMean(offer(dahab, [diving, snorkeling, horseRiding], 10000, 2020-02-12, 2020-03-12, period(2020-03-15, 2020-04-15), 10, 5), bus).
+offerMean(offer(taba, [diving], 1000, 2020-02-12, 2020-03-12, period(2020-06-01, 2020-08-31), 10, 4), bus).
+
+% offerAccommodation(X, Y) -> Accommodation Y is part of offer X
+
+offerAccommodation(offer(dahab, [diving, snorkeling, horseRiding], 10000, 2020-02-12, 2020-03-12, period(2020-03-15, 2020-04-15), 10, 5), hotel).
+offerAccommodation(offer(taba, [diving], 1000, 2020-02-12, 2020-03-12, period(2020-06-01, 2020-08-31), 10, 4), hotel).
+
+% customerPreferredActivity(X, Y, R) -> Y is the preferred activity kind wrt customer X with relevance R
+
+customerPreferredActivity(customer(ahmed, aly, 1993-01-30, single, 0, student), diving, 100).
+customerPreferredActivity(customer(ahmed, aly, 1993-01-30, single, 0, student), snorkeling, 100).
+customerPreferredActivity(customer(ahmed, aly, 1993-01-30, single, 0, student), horseRiding, 20).
+customerPreferredActivity(customer(mohamed, elkasad, 1999-01-30, single, 0, student), snorkeling, 60).
+customerPreferredActivity(customer(mohamed, elkasad, 1999-01-30, single, 0, student), diving, 20).
+customerPreferredActivity(customer(mohamed, elkasad, 1999-01-30, single, 0, student), horseRiding, 50).
+
+% customerPreferredMean(X, Y, R) -> Y is the preferred transportaion mean wrt customer X with relevance R
+
+customerPreferredMean(customer(ahmed, aly, 1993-01-30, single, 0, student), bus, 100).
+customerPreferredMean(customer(mohamed, elkasad, 1999-01-30, single, 0, student), bus, 10).
+
+% customerPreferredAccommodation(X, Y, R) -> Y is the preferred accommodation to customer X with relevance R
+
+customerPreferredAccommodation(customer(ahmed, aly, 1993-01-30, single, 0, student), hotel, 20).
+customerPreferredAccommodation(customer(mohamed, elkasad, 1999-01-30, single, 0, student), hotel, 100).
